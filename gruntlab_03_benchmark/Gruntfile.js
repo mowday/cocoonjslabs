b514bf8a3b9a5ca10bc7f7214df7d26a57ac317c
Carl Mowday <carl.mowday@playngo.com>
2014-08-17 11:01:20 +0200

    Further labs

--- gruntlab_03_benchmark/Gruntfile.js
+++ gruntlab_03_benchmark/Gruntfile.js
@@ -25,34 +25,39 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    
-    uglify: {
-      options: {
-        banner: banner
-      },
-      build: {
-        src: srcFiles,
-        dest: 'build/<%= pkg.name %>.min.js'
+
+    jasmine: {
+      test: {
+        src: 'src/*.js',
+        options: {
+          specs: 'test/*.spec.js',
+          display: 'full'
+        }
       }
     },
 
 
-    jshint: {
-      source: {
-        src: srcFiles
+    benchmark: {
+      all: {
+        src: ['benchmarks/*.js'],
+        dest: 'benchmarks/results.csv'
+      },
+
+      options: {
+        displayResults: true
       }
     }
 
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-benchmark');
 
   // Default task(s).
   grunt.registerTask('default', [
-    'uglify',
-    'jshint:source'
+    //'jasmine',
+    'benchmark'
   ]);
 
 };
